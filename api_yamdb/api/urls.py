c18113826de0dbbd7ba0f2996e972c9bc0cc43f4
RuslanAkhmejanov <akhmejanov.r@gmail.com>
2021-09-17 17:18:26 +0600

    Auth and Users Done

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api_yamdb.reviews.views import CategoriesViewSet, GenresViewSet, TitlesViewSet
+from reviews.views import CategoriesViewSet, GenresViewSet, TitlesViewSet
+from users.views import register_user, update_user_info, UserList, UserDetail
+
 
 router_ver1 = DefaultRouter()
 
@@ -11,4 +13,8 @@ router_ver1.register(r'titles', TitlesViewSet, basename='titles')
 
 urlpatterns = [
     path('v1/', include(router_ver1.urls)),
+    path('v1/auth/signup/', register_user),
+    path('v1/users/me/', update_user_info),
+    path('v1/users/<str:username>/', UserDetail.as_view()),
+    path('v1/users/', UserList.as_view()),
 ]
